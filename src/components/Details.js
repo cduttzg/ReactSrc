0a48d858f5a431602e7f80583e778e04c3ad028a
JankinLee <37661555+JankinLee@users.noreply.github.com>
2019-07-03 17:42:27 +0800

    Update Details&ShowCart

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react'
 import {BrowserRouter as Router, Link} from 'react-router-dom'
-import { Layout, Breadcrumb, Card, Avatar, Col, Row, Button } from 'antd'
+import { Layout, Breadcrumb, Card, Avatar, Col, Row, Button, Modal, Icon, message } from 'antd'
 import '../styleSheets/Details.css'
 import { thisExpression } from '@babel/types';
+import { relative } from 'path';
 
 export default class Details extends Component{
     constructor(props){
@@ -11,6 +12,7 @@ export default class Details extends Component{
             sellerName:"灰太狼",
             sellerAvatar:"xxx",
             sellerSoldNum: 999,
+            sellerPaymentCode: "ooo",
             btnDisable1: false,
             btnDisable2: false,
             loading:false,
@@ -22,6 +24,8 @@ export default class Details extends Component{
             sellerPhoneNum:"17766377737",
             goodsNum:1,
             goodsStock:10,
+            visible: false,
+            success: false
         }
         this.numChange1=this.numChange1.bind(this);
         this.numChange2=this.numChange2.bind(this);
@@ -61,11 +65,61 @@ export default class Details extends Component{
     }
 
     enterLoading = () => {
-        this.setState({ loading: true });
-    };
-    
-    render(){
+        if(this.state['goodsNum']==0){
+            message.error('商品数量为0...你到底买不买啊!(｡•ˇ‸ˇ•｡)',2.5);
+        }else{
+            this.setState({ loading: true});
         
+            setTimeout(() => {
+              this.setState({
+                loading: false,
+                visible: true 
+              });
+            }, 500);
+        }
+    };
+
+    //点击OK
+    handleOk = e => {
+        console.log(e);
+        if(this.state['goodsNum']==0){
+            message.error('商品数量为0,你到底买不买啊!(｡•ˇ‸ˇ•｡)',2.5);
+        }else{
+            this.state['success']?message.loading("正在提交...",0.5).then(()=>message.success('您已经完成支付! 卖家会在24小时内尽快联系您๑乛◡乛๑',2.5)):message.loading("正在提交...",0.5).then(()=>message.error('很抱歉,创建订单失败!我们将尽快进行处理!!!∑(ﾟДﾟノ)ノ',2.0));
+            //需要调用创建订单的API
+
+            this.setState({
+                visible: false,
+            });
+        }
+    };
+
+    //点击取消
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+    };
+
+
+    onChange(a, b, c) {
+        console.log(a, b, c);
+    }
+    
+    //添加商品至购物车
+    addGoods = e => {
+        if(this.state['goodsNum']==0){
+            message.error('商品数量为0...你到底买不买啊!(｡•ˇ‸ˇ•｡)',2.5);
+        }else{
+            //调用添加商品到购物车的API
+            this.state['success']?message.loading("正在提交...",0.5).then(()=>message.success('已成功添加到购物车,快去购物车看看宝贝吧~(っ ̯ -｡) ',2.5)):message.loading("正在提交...",0.5).then(()=>message.error('添加到购物车失败！我们记录错误并尽快解决问题!!!∑(ﾟДﾟノ)ノ',2.5));
+        }
+        
+    }
+
+    render(){
+        const { Meta } = Card;
         const ButtonGroup = Button.Group;
         const post = {//this.props.locations.state.
             goodsId:"007",
@@ -87,7 +141,7 @@ export default class Details extends Component{
                         <Breadcrumb separator=">"style={{ margin: '16px 0' }}>
                             <Link to=""><Breadcrumb.Item>首页</Breadcrumb.Item></Link>
                             <Link to=""><Breadcrumb.Item>分类名</Breadcrumb.Item></Link>
-                            <Breadcrumb.Item>商品名</Breadcrumb.Item>
+                            <Breadcrumb.Item>{this.state['goodsName']}</Breadcrumb.Item>
                         </Breadcrumb>
                     </Router>
                     <div className="main" style={{ background: '#fff', padding: 24, minHeight: 280 }}>
@@ -97,16 +151,15 @@ export default class Details extends Component{
                                 <Layout>
                                     <Content>
                                         <div id="goodsInfo">
-                                            <Card title={this.state['goodsName']}>
-                                                <Card type="inner" bordered={false} title="商品描述">
+                                            <Card title={this.state['goodsName']} style={{ marginTop: 16 , textAlign: "center"}}>
+                                                <Card type="inner" bordered={false} title="商品描述" style={{ marginTop: 16 , textAlign: "center"}}>
                                                     {this.state['goodsInfo']}
                                                 </Card>
                                                 <Card
                                                     bordered={false}
-                                                    style={{ marginTop: 16 }}
+                                                    style={{ marginTop: 16 , textAlign: "center"}}
                                                     type="inner"
                                                     title="卖家信息"
-                                                    extra={<a href="#">更多</a>}
                                                 >
                                                     <Row>
                                                         <Col span={8}>
@@ -160,16 +213,39 @@ export default class Details extends Component{
                                         </Col>
                                         <Col span={2}>
                                             <div className="OperatorsBox">
-                                            <Button type="primary" shape="round" icon="shopping-cart" size="large">
-                                                进入购物车
+                                            <Button type="primary" shape="round" icon="shopping-cart" size="large" onClick={this.addGoods}>
+                                                添加至购物车
                                             </Button>
                                             </div>
                                         </Col>
                                         <Col span={3}>
                                             <div className="OperatorsBox">
-                                            <Button type="primary" shape="round" icon="pay-circle" size="large" loading={this.state['loading']} onClick={this.enterLoading}>
-                                                立即购买
-                                            </Button>
+                                                <Button type="primary" shape="round" icon="pay-circle" size="large" loading={this.state['loading']} onClick={this.enterLoading}>
+                                                    立即购买
+                                                </Button>
+                                                <Modal
+                                                    title="付款窗口"
+                                                    visible={this.state['visible']}
+                                                    onOk={this.handleOk}
+                                                    onCancel={this.handleCancel}
+                                                >
+                                                    <Card
+                                                        style={{ width: '100%'}}
+                                                        cover={
+                                                        <img
+                                                            alt="付款码"
+                                                            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"//this.state.sellerPaymentCode
+                                                        />
+                                                        }
+                                                        actions={[<img src={this.state['goosImg']} alt={this.state['goodsName']} />]}
+                                                    >
+                                                        <Meta
+                                                        avatar={/*卖家头像 */<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                                        title={this.state['sellerName']}
+                                                        description={this.state["goodsName"]}
+                                                        />
+                                                    </Card>
+                                                </Modal>
                                             </div>
                                         </Col>
                                     </Row>
--- src/components/ShowCart.js
+++ src/components/ShowCart.js
@@ -1,8 +1,8 @@
 import React,{Component} from 'react'
-import {Layout, Row, Col, Card, Checkbox, Button, Modal } from 'antd'
+import {Layout, Row, Col, Card, Checkbox, Button, Modal, Steps, message } from 'antd'
 import '../styleSheets/ShowCart.css'
-const CheckboxGroup = Checkbox.Group;
 const{confirm}=Modal;
+const{Step} = Steps;
 
 export default class ShowCart extends Component{
 
@@ -16,6 +16,7 @@ export default class ShowCart extends Component{
           indeterminate:false,
           checkAll:false,
           sellerName:["小白","小红","小灰","小黑"],
+          sellerPaymentCode:"xxx",
           plainOptions:[0,1,2,5],
           goodsInfo:["小白的毛衣","小红的帽子","小灰的裤子","小黑鞋子"],
           goodsPrice:[11,22,33,44],
@@ -24,6 +25,9 @@ export default class ShowCart extends Component{
           btnDisable1:[false,false,false,false],
           btnDisable2:[false,false,false,false],
           loading: false,
+          visible: false,
+          current:0,
+          success:false
       }
   }
 
@@ -146,31 +150,86 @@ export default class ShowCart extends Component{
       okText: '确认',
       cancelText: '关闭',
       onOk() {
-        console.log('OK');
+        message.success('该商品已删除_(:ι」∠)_',1.5);
       },
       onCancel() {
         console.log('Cancel');
       },
     });
   }
+
+  handleOk = e => {
+      console.log(e);
+      //调用创建订单API,返回success
+      this.state.success?message.loading("正在提交...",0.5).then(()=>message.success('您已经完成支付! 卖家会在24小时内尽快联系您๑乛◡乛๑',2.5)):message.loading("正在提交...",0.5).then(()=>message.error('很抱歉,创建订单失败!我们将尽快进行处理!!!∑(ﾟДﾟノ)ノ',2.0));
+      this.setState({
+          visible: false,
+      });
+  };
+
+  handleCancel = e => {
+      console.log(e);
+      this.setState({
+        visible: false,
+      });
+  };
   
   handleClick = () => {
     this.setState({
       loading:true,
+      
     });
     setTimeout(() => {
       this.setState({
         loading: false,
+        visible:true
       });
-    }, 2000);
-  };
-  
-  enterLoading = () => {
-    this.setState({ loading: true });
+    }, 500);
   };
 
+  next() {
+    const current = this.state['current'] + 1;
+    this.setState({ current });
+  }
+
+  prev() {
+    const current = this.state['current'] - 1;
+    this.setState({ current });
+  }
+
+  handleDone = e => {
+      console.log(e);
+      this.state.success?message.loading("正在提交...",0.5).then(()=>message.success('您已经完成支付! 卖家会在24小时内尽快联系您๑乛◡乛๑',2.5)):message.loading("正在提交...",0.5).then(()=>message.error('很抱歉,创建订单失败!我们将尽快进行处理!!!∑(ﾟДﾟノ)ノ',2.0));
+      this.setState({
+          visible: false,
+      });
+  };
+
+
   render(){
+    const steps=[
+      {
+        title:"小白",
+        content: <img src={this.state['sellerPaymentCode']} alt={"收款码"}/>
+      },
+      {
+        title:"小红",
+        content: <img src={this.state['sellerPaymentCode']} alt={"收款码"}/>
+      },
+      {
+        title:"小灰",
+        content: <img src={this.state['sellerPaymentCode']} alt={"收款码"}/>
+      },
+      {
+        title:"小黑",
+        content: <img src={this.state['sellerPaymentCode']} alt={"收款码"}/>
+      }
+    ];
+    //当前付款卖家索引
+    const{current}=this.state;
+    //布局内容
     const {Content} = Layout;
+    //按钮组
     const ButtonGroup=Button.Group;
     return(
       <Layout> 
@@ -227,6 +286,38 @@ export default class ShowCart extends Component{
           <Button type="primary" shape="round" icon="pay-circle" size="large" loading={this.state['loading']} onClick={this.handleClick}>
             立即购买
           </Button>
+          <Modal
+            title="付款窗口"
+            visible={this.state['visible']}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <div>
+              <Steps current={current}>
+                {steps.map(item => (
+                  <Step key={item.title} title={item.title} />
+                ))}
+              </Steps>
+              <div className="steps-content">{steps[current].content}</div>
+              <div className="steps-action">
+                {current < steps.length - 1 && (
+                  <Button type="primary" onClick={() => this.next()}>
+                    为下一件商品付款
+                  </Button>
+                )}
+                {current === steps.length - 1 && (
+                  <Button type="primary" onClick={this.handleDone}>
+                    完成
+                  </Button>
+                )}
+                {current > 0 && (
+                  <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+                    返回上一件商品
+                  </Button>
+                )}
+              </div>
+            </div>
+          </Modal>
         </div>
         </Content>
       </Layout>
